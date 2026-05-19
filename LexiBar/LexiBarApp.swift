21c4347700274432af4b351874d8db36a33eab22
Andrey Pudov <andrey.pudov@supergroup.com>
2026-05-19 14:41:28 +0200

    Initial application skeleton

---
+++ LexiBar/LexiBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  LexiBarApp.swift
+//  LexiBar
+//
+//  Created by Andrey Pudov on 19/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct LexiBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
