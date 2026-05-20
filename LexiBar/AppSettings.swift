4c05a2a6d34d90a12427ca3498c5e8ad02fe5151
Andrey Pudov <andrey.pudov@supergroup.com>
2026-05-20 17:59:25 +0200

    Added vocabulary loading and displaying

---
+++ LexiBar/AppSettings.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+class AppSettings {
+    static let shared = AppSettings()
+
+    private enum Keys {
+        static let wordInterval = "wordInterval"
+    }
+
+    private enum Defaults {
+        static let wordInterval: TimeInterval = 5
+    }
+
+    var wordInterval: TimeInterval {
+        get {
+            let stored = UserDefaults.standard.double(forKey: Keys.wordInterval)
+            return stored > 0 ? stored : Defaults.wordInterval
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: Keys.wordInterval)
+        }
+    }
+}
