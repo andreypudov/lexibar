130d4d8f94cffcc02b721429528936ae5280ac2e
Andrey Pudov <andrey.pudov@supergroup.com>
2026-05-20 23:35:10 +0200

    ~ changed timeout to six seconds

--- LexiBar/AppSettings.swift
+++ LexiBar/AppSettings.swift
@@ -8,7 +8,7 @@ class AppSettings {
     }
 
     private enum Defaults {
-        static let wordInterval: TimeInterval = 5
+        static let wordInterval: TimeInterval = 6
     }
 
     var wordInterval: TimeInterval {
